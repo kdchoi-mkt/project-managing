d63bda5a2844a3f20ee30509ddd063b74524cbe3
kdchoi-mkt <valuekds@naver.com>
2020-08-19 03:16:19 +0900

    Add MockData

---
+++ managingApplication/src/global/mockData.js
@@ -0,0 +1,26 @@
+export const task = [
+    {
+        id: 1,
+        user_id: 1,
+        schedule_id: 1,
+        title: "안녕하세요, 테스트입니다",
+        description: "테스트임을 알려드립니다.",
+        is_completed: true
+    },
+    {
+        id: 2,
+        user_id: 1,
+        schedule_id: 1,
+        title: "안녕하세요, 테스트입니다2",
+        description: "테스트임을 알려드립니다.",
+        is_completed: false
+    },
+    {
+        id: 3,
+        user_id: 1,
+        schedule_id: 1,
+        title: "안녕하세요, 테스트입니다3",
+        description: "테스트임을 알려드립니다.",
+        is_completed: false
+    }
+]
