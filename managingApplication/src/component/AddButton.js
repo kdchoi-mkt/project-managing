b2e20642a6ad55f31e74728c39fc27609cab36ff
kdchoi-mkt <valuekds@naver.com>
2020-08-18 14:51:46 +0900

    make the backbone of confirm button

--- managingApplication/src/component/AddButton.js
+++ managingApplication/src/component/AddButton.js
@@ -1,4 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
-    TouchableOpacity
+    TouchableOpacity, StyleSheet
 } from 'react-native'
+
+const AddButton = (props) => {
+    const [overwriteButtonStyle, setButtonStyle] = useState({backgroundColor: 'white'})
+
+    const onPressButton = () => {
+        // props.press()
+
+        // 밑의 내용은 모두 DB에서 가져오는 것으로 처리해야함
+        let modifier = overwriteButtonStyle.backgroundColor === 'green' ? 'white' : 'green'
+        setButtonStyle({backgroundColor: modifier})
+    }   
+
+    return (
+        <TouchableOpacity style = {[props.styles, styles.buttonStyle, overwriteButtonStyle]}
+                          onPress = {onPressButton} />
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonStyle: {
+        height: 30,
+        width: 30,
+        borderColor: 'black',
+        borderWidth: 5        
+    }
+})
+
+export default AddButton
